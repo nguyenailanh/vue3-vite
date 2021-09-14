a1987c99669cbda68fb3f345f79c5296a8dfeec9
Lanh Nguyen <lanhnguyen@Lanhs-MacBook-Air.local>
2021-09-14 09:17:02 +0700

    add pre-commi hook

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,9 @@
 module.exports = {
-	extends: ['plugin:vue/vue3-essential', 'prettier'],
+	extends: ['plugin:vue/vue3-essential', 'plugin:prettier/recommended'],
+	plugins: ['prettier'],
 	rules: {
 		// override/add rules settings here, such as:
 		'vue/no-unused-vars': 'error',
+		'prettier/prettier': 'error',
 	},
 }
